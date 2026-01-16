8144922d17af107f1ecd9b0fa9778ab32e27f69a
pahae-tests <wahipahae@gmail.com>
2026-01-16 17:00:29 +0100

    Update getMatches.js

--- src/pages/api/getMatches.js
+++ src/pages/api/getMatches.js
@@ -8,12 +8,63 @@ export default async function handler(req, res) {
 
   try {
     await dbConnect();
-    const matches = await Match.find({})
+    let matches = await Match.find({})
       .populate('goals.scorer', 'name img')
       .populate('goals.assister', 'name img')
       .sort({ date: -1 });
+
+    matches = matches.map(match => {
+      const scoreParts = match.score.split("-").map(Number);
+      const totalGoalsUs = scoreParts[0];
+      const totalGoalsThem = scoreParts[1];
+
+      const playersSet = new Map();
+      match.goals.forEach(g => {
+        if (g.scorer) playersSet.set(g.scorer._id.toString(), { ...g.scorer.toObject(), number: g.num });
+        if (g.assister) playersSet.set(g.assister._id.toString(), { ...g.assister.toObject(), number: g.num });
+      });
+
+      const players = Array.from(playersSet.values()).map(player => {
+        let goals = 0, assists = 0;
+        match.goals.forEach(g => {
+          if (g.scorer?._id.toString() === player._id.toString()) goals++;
+          if (g.assister?._id.toString() === player._id.toString()) assists++;
+        });
+
+        let goalsCoef = 1.4, assistCoef = 1.1;
+        if (Math.abs(totalGoalsUs - totalGoalsThem) === 1) {
+          if (match.goals[match.goals.length - 1]?.scorer?._id.toString() === player._id.toString()) {
+            goalsCoef += 5 / totalGoalsUs;
+            assistCoef += 2.5 / totalGoalsUs;
+          } else {
+            goalsCoef += 0.6;
+            assistCoef += 0.4;
+          }
+        }
+
+        let rating = 5 + (goals * goalsCoef) + (assists * assistCoef);
+        if (rating > 10) rating = 10;
+
+        return {
+          ...player,
+          goals,
+          assists,
+          rating: Number(rating.toFixed(1)),
+        };
+      });
+
+      const maxRating = Math.max(...players.map(p => p.rating));
+
+      return {
+        ...match.toObject(),
+        players,
+        mvp: players.find(p => p.rating === maxRating) || null
+      };
+    });
+    
     res.status(200).json({ success: true, data: matches });
   } catch (error) {
+    console.log(error)
     res.status(400).json({ success: false, error: error.message });
   }
 }
