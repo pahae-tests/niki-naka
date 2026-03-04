e01d1ffdf673ba463077f9c7bb24f17bec1adf11
pahae-tests <wahipahae@gmail.com>
2026-03-04 18:39:55 +0000

    Update getMatches.js

--- src/pages/api/getMatches.js
+++ src/pages/api/getMatches.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
         }
 
         let rating = 5 + (goals * goalsCoef) + (assists * assistCoef);
-        let rr = rating;
+        let rr = Number(rating);
         if (rating > 10) rating = 10;
 
         return {
@@ -73,3 +73,4 @@ export default async function handler(req, res) {
   }
 
 }
+
